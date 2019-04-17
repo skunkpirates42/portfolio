51e457634546d818c8932f00395b0fcfb8d1cf9b
Peter Ramos <peterdramos02@gmail.com>
2019-04-17 19:53:16 -0400

    add javascript for scrolling behavior

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
+// Console Welcome Statements
+console.log(
+  'Hello, nice to meet you! Checkout my github and linkedin!'
+);
+console.log('https://github.com/skunkpirates42');
+console.log('https://www.linkedin.com/in/peterramos2/');
+
+
+// Scroll Behavior
+function anchorLinkHandler(e) {
+  const distanceToTop = el => Math.floor(el.getBoundingClientRect().top);
+
+  e.preventDefault();
+  const targetID = this.getAttribute('href');
+  const targetAnchor = document.querySelector(targetID);
+  if (!targetAnchor) return;
+  const originalTop = distanceToTop(targetAnchor);
+
+  window.scrollBy({ top: originalTop, left: 0, behavior: 'smooth' });
+
+  const checkIfDone = setInterval(function() {
+    const atBottom =
+      window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 2;
+    if (distanceToTop(targetAnchor) === 0 || atBottom) {
+      targetAnchor.tabIndex = '-1';
+      targetAnchor.focus();
+      window.history.pushState('', '', targetID);
+      clearInterval(checkIfDone);
+    }
+  }, 125);
+}
+
+const linksToAnchors = document.querySelectorAll('a[href^="#"]');
+
+linksToAnchors.forEach(each => (each.onclick = anchorLinkHandler));
+
+// Dynamic Year
 const year = new Date().getFullYear();
 const yearEl = document.querySelector('#js-year');
 yearEl.innerHTML = year;
